4be340e92b43e0db37aa638a583e4c27092552ce
woorej <wjk5848@gmail.com>
2023-01-12 16:31:09 +0900

    uploaded to main from temp

--- data_loader/TTA_sample/temp.py
+++ data_loader/TTA_sample/temp.py
@@ -1,60 +1,60 @@
-import os
 import json
+import os
+import random
+
+class CustomDataLoader() :
+    def __init__(self, path : str, main_category : str, want_items : list = None, option=None) :
+        
+        self.main_category = main_category
+        self.path = path
+        self.second_category_list = self.get_second_category(self.path, self.main_category)
+        self.third_category_list = self.get_third_category(self.path, self.main_category, self.second_category_list)
+        self.ImageID = self.get_json(self.path, self.main_category, self.second_category_list, self.third_category_list)
+
+    def get_ImageID(self) :
+        print(self.ImageID) 
+
+    def get_json(self, path, main_category, second_category_list, third_category_list):
+        result = {}
+        for i in range(len(second_category_list)): # 2
+            image_id = []
+            for j in range(len(third_category_list[i])):
+                json_path = path + main_category + '/' + second_category_list[i] + '/' + third_category_list[i][j] + '/' + 'info.json'
+                with open(json_path, 'r') as f:
+                    json_data = json.load(f)
+                    jsonArray =  json_data.get("data")
+                    for st in jsonArray:
+                        image_id.append(st.get("ImageID")) # 원하는 품목
+                        #image_id.append(st.get("Filename")) # 원하는 품목
+                                    
+            result[second_category_list[i]] = image_id
+
+        real_result = {}
+        real_result[main_category] = result
+
+        return real_result
 
 
-def get_json(path, main_category, second_category_list, third_category_list):
-    result = {}
-    for i in range(len(second_category_list)): # 2
-        image_id = []
-        for j in range(len(third_category_list[i])):
-            json_path = path + main_category + '/' + second_category_list[i] + '/' + third_category_list[i][j] + '/' + 'info.json'
-            with open(json_path, 'r') as f:
-                json_data = json.load(f)
-                jsonArray =  json_data.get("data")
-                for st in jsonArray:
-                    image_id.append(st.get("ImageID"))
-                                   
-        result[second_category_list[i]] = image_id
-    return result
+    def get_second_category(self, path, main_category):
+        root_dir = os.path.join(path, main_category)
+        root_under_dir_list = os.listdir(root_dir)
 
-def get_second_category(path, main_category):
-    root_dir = os.path.join(path, main_category)
-    root_under_dir_list = os.listdir(root_dir)
-    return root_under_dir_list   # H4131_스피커, H4141_헤드폰
+        return root_under_dir_list   # H4131_스피커, H4141_헤드폰
 
 
-def get_third_category(path, main_category, second_category_list): # IT ,['H4131_스피커', 'H4141_헤드폰']
+    def get_third_category(self, path, main_category, second_category_list): # IT ,['H4131_스피커', 'H4141_헤드폰']
+        third_catory_list = []
+        for i in range(len(second_category_list)):
+            root_dir = os.path.join(path+main_category,second_category_list[i]) # './ML_Tutorial/data_loader/TTA_sample/IT' + ['H4131_스피커', 'H4141_헤드폰']
+            root_under_dir_list = os.listdir(root_dir) #  ['스피커_Beosound_Balance', '스피커_ANIMO', '스피커_AXIS_C1410_Network_Mini_Speaker'..]
+            third_catory_list.append(root_under_dir_list)
+
+        return third_catory_list
+
     
-    third_catory_list = []
-    for i in range(len(second_category_list)):
-        root_dir = os.path.join(path+main_category,second_category_list[i]) # './ML_Tutorial/data_loader/TTA_sample/IT' + ['H4131_스피커', 'H4141_헤드폰']
-        root_under_dir_list = os.listdir(root_dir) #  ['스피커_Beosound_Balance', '스피커_ANIMO', '스피커_AXIS_C1410_Network_Mini_Speaker'..]
-        third_catory_list.append(root_under_dir_list)
-
-    return third_catory_list
-
-
-
-# 요기가 이제 main function 이지 않을까
 path = './TTA_sample/'
+main_category='test_item'
 
-main_category = 'test_item'
+CustomDataLoader(path, main_category).get_ImageID()
 
-second_category_list = get_second_category(path, main_category)
-'''
-['H4131_스피커', 'H4141_헤드폰']
-'''
-third_category_list = get_third_category(path, main_category, second_category_list)
-'''
-[['스피커_Beosound_Balance', '스피커_ANIMO', '스피커_AXIS_C1410_Network_Mini_Speaker', '스피커_BEOSOUND_EMERGE', '스피커_BALMUDA_The_Speaker'], 
-['헤드폰_AKG_K371_헤드폰', '헤드폰_AKG_K175_프로페셔널_접이식_밀폐형', '헤드폰_Awei_A790BL', '헤드폰_akg_headphone_y500bt', '헤드폰_Apple_에어팟_맥스']]
-'''
-image_ID = get_json(path, main_category, second_category_list, third_category_list)
 
-result = {}
-result[main_category] = image_ID
-   
-#print('H4131_스피커 개수 :', len(result['IT']['H4131_스피커']))
-#print('H4141_헤드폰 개수 :', len(result['IT']['H4141_헤드폰']))
-
-print(result)
