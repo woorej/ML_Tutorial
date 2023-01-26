4dca45c68841d280b6c737451c871dd54bc8dc63
woorej <wjk5848@gmail.com>
2023-01-26 14:50:51 +0900

    onnx_mlir_c

---
+++ onnx_mlir_inference/OMInstrument.h
@@ -0,0 +1,72 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===------------- OMInstrument.h - OM Instrument Declaration header ------===//
+//
+// Copyright 2019-2020 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains declaration of API functions for instrumentation.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef ONNX_MLIR_OMINSTRUMENT_H
+#define ONNX_MLIR_OMINSTRUMENT_H
+
+#ifdef __cplusplus
+#include <algorithm>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <string>
+#include <vector>
+#else
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#endif // #ifdef __cplusplus
+
+#if defined(__APPLE__) || defined(__MVS__)
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#endif // #ifdef __APPLE__
+
+#include "OMCompilerMacros.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Initialize instrument.
+ * Initialize counter and read env variables for control
+ *
+ */
+OM_EXTERNAL_VISIBILITY void OMInstrumentInit();
+
+/**
+ * Create an instrument point.
+ * Measurement of runtime behavior will be measured and output
+ * In current implementation, the elapsed time from previous instrument point,
+ * and virtual memory size will be reported.
+ *
+ * @param id for this point. op name is used now.
+ * @param tag can used to give extra control of output. Used for begin/end mark
+ * now
+ * @param nodeName is an onnx_node_name attribute in the graph.
+ * @return void
+ *
+ */
+OM_EXTERNAL_VISIBILITY void OMInstrumentPoint(
+    const char *opName, int64_t tag, const char *nodeName);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ONNX_MLIR_OMINSTRUMENT_H
---
+++ onnx_mlir_inference/OMSignature.h
@@ -0,0 +1,42 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===----------- OMSignature.h - OMSignature Declaration header -----------===//
+//
+// Copyright 2021 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains declaration of OMSignature API functions.
+//
+//===----------------------------------------------------------------------===//
+
+#include "OMCompilerMacros.h"
+
+#ifdef __cplusplus
+#pragma once
+
+extern "C" {
+#endif
+
+/**
+ * \brief Return the input signature of the given entry point as a JSON string.
+ *
+ * @param entry point name
+ * @return pointer to input signature JSON string
+ */
+OM_EXTERNAL_VISIBILITY const char *omInputSignature(const char *entryPointName);
+
+/**
+ * \brief Return the output signature of the given entry point as a JSON string.
+ *
+ * @param entry point name
+ * @return pointer to output signature JSON string
+ */
+OM_EXTERNAL_VISIBILITY const char *omOutputSignature(
+    const char *entryPointName);
+
+#ifdef __cplusplus
+}
+#endif
